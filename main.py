9bb04b031be58c252ae0990cf49678441b3ac5ad
Nick Herrig <neherrig@gmail.com>
2024-02-15 11:34:12 -0600

    Add toml config files for sensative values

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import cv2
 import requests
+from functools import partial
+
+
+import onnxruntime as ort
+import supervision as sv
+import toml
+
 from inference import InferencePipeline
 from inference.core.interfaces.stream.sinks import render_boxes
-import onnxruntime as ort
 
 
-def send_alert(message):
+bounding_box = sv.BoundingBoxAnnotator()
+label = sv.LabelAnnotator()
+
+
+def send_alert(alert_endpoint):
     """
     Send an alert message to a specified URL.
 
@@ -16,46 +27,67 @@ def send_alert(message):
 
     Returns:
     Response: The response from the server after the post request is made.
-    """
-    return requests.post(
-        "", # Redacted 
-        data=message.encode(encoding='utf-8')
-    ) 
+    """    
+    requests.post(alert_endpoint,
+        data="Someone is at your desk",
+        headers={
+            "Title": "Unauthorized access detected",
+            "Priority": "urgent",
+            "Tags": "warning,skull"
+        })
+ 
+
+
+def on_prediction(inference_results, frame):
+
+    detections = sv.Detections.from_inference(inference_results)
+
+    labels = [f"class: {class_id}" 
+              for class_id 
+              in detections.class_id]
+
+    annotated_frame = label.annotate(scene=frame.image.copy(), detections=detections, labels=labels)
+    annotated_frame = bounding_box.annotate(scene=annotated_frame, detections=detections)
+
+    cv2.imshow("Inference", annotated_frame)
+    cv2.waitKey(1)
 
 
 def main():
 
     print("Available Providers: ", ort.get_available_providers())
 
-    rtsp_user = "" # Redacted
-    rtsp_password = "" # Redacted
-    camera_ip = "" # Redacted
+
+    # Load configuration file and parse values
+    config = toml.load('config.toml')
+    rtsp_user = config["rtsp"]["user"]
+    rtsp_pass = config["rtsp"]["password"]
+    camera_ip = config["rtsp"]["camera_ip"]
+    rtsp_port = config["rtsp"]["port"]
+
+    # Use rtsp values to create the rtsp url for use in inference pipeline
+    rtsp_url = f"rtsp://{rtsp_user}:{rtsp_pass}@{camera_ip}:{rtsp_port}/h264Preview_01_main"
 
 
+    # Load additional configuration for Inference pipeline
+    active_learning = config["app"]["active_learning"]
+    roboflow_api_key = config["app"]["roboflow_api_key"]
+    roboflow_model = config["app"]["roboflow_model"]
+    roboflow_model = config["app"]["roboflow_model"]
+    alert_endpoint = config["app"]["alert_endpoint"]
+
+
+    # Initialize the Inference pipeline
     pipeline = InferencePipeline.init(
-        model_id="coco/6",
-        video_reference=f"rtsp://{rtsp_user}:{rtsp_password}@{camera_ip}:554/h264Preview_01_main", 
-        on_prediction=render_boxes,
-        api_key="", # Redacted
-        max_fps=10,
-        confidence=0.75
+        model_id=roboflow_model,
+        api_key=roboflow_api_key,
+        video_reference=rtsp_url, 
+        on_prediction=on_prediction,
+        active_learning_enabled=active_learning,
     )
 
     pipeline.start()
     pipeline.join()
-
-
-    # TODO Hand Signal For Enabling Lock
-
-    # TODO Hand Signal Unlocking the Area
-
-    # There will be some global state if the area is locked/unlocked. 
-
-    # If the desk is Locked, start alert timer, and watch for  
-
-    # We'll want a tracker to track hand signals, and people. 
-
-    # The main app will detect people in the space, and when detected will kick off an alert timer. 
     
 
 if __name__ == "__main__":
