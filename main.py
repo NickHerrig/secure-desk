1d74e1902c63dc6eb6e11aef7475edcb0185367d
Nick Herrig <neherrig@gmail.com>
2024-02-15 19:29:37 -0600

    App v0.01 completed, needs cleaning up but works

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ import toml
 from inference import InferencePipeline
 from inference.core.interfaces.stream.sinks import render_boxes
 
-
 # global supervision annnotators
 bounding_box = sv.BoundingBoxAnnotator()
-label = sv.LabelAnnotator()
+label = sv.LabelAnnotator(text_scale=3, text_thickness=2)
 tracker = sv.ByteTrack()
 
 # global application logic state
@@ -22,6 +21,9 @@ should_send_alert = True
 code_sequence = [2, 0]
 current_code_index = 0
 tracked_codes = set()
+desk_unlocked = False
+alert_state_message = ""
+code_sequence_message = ""
 
 
 def check_code_sequence(tracked_detections):
@@ -29,39 +31,51 @@ def check_code_sequence(tracked_detections):
     Check if the detected prediction matches the next expected code in the sequence.
     If the sequence is fully matched, disable the alert.
     """
-    global current_code_index, should_send_alert
+    global current_code_index, should_send_alert, code_sequence_message
 
     class_ids = tracked_detections.class_id
 
     if code_sequence[current_code_index] in class_ids:
         current_code_index += 1
-        logging.info(f"Correct code entered. Step {current_code_index} of {len(code_sequence)} completed.")
-        
+        msg = f"Correct code entered. Step {current_code_index} of {len(code_sequence)} completed."
+        logging.info(msg)
+        code_sequence_message = msg
         if current_code_index == len(code_sequence):
-            logging.info("Correct sequence entered. Desk is unlocked!")
+
+            global desk_unlocked
+            desk_unlocked = True
+            msg = "Correct sequence entered! Desk is unlocked!"
+            logging.info(msg)
+            code_sequence_message = msg
             should_send_alert = False
-            # Reset for next time
             current_code_index = 0
     else:
         # If the code is incorrect, reset the sequence
         if current_code_index > 0:
-            logging.info(f"Incorrect code entered. Resetting sequence.")
+            msg = "Incorrect code entered. Resetting sequence."
+            logging.info(msg)
+            code_sequence_message = msg
             current_code_index = 0
 
 
 def alert_timer(alert_endpoint):
 
-    logging.info("You have 10 seconds to crack the code...")
+    for i in range(20, 0, -1):
+        global alert_state_message
+        alert_state_message = f"{i} seconds left."
+        time.sleep(1)
 
-    time.sleep(15)
-
+        global desk_unlocked
+        if desk_unlocked:
+            break
+    
     global should_send_alert
     if should_send_alert:
-        logging.info("Intruder detected, sending alert")
         send_alert(alert_endpoint)
-    else:
-        logging.info("Desk is unlocked! ")
+        alert_state_message =  "Alert Sent!"
 
+    else:
+        alert_state_message = "Desk Unlocked!"
 
 def send_alert(alert_endpoint):
     """
@@ -92,7 +106,7 @@ def on_prediction(alert_endpoint, inference_results, frame):
 
     class_names = getattr(detections, 'data', {}).get('class_name', []) if hasattr(detections, 'data') else []
 
-    labels = [f"{class_name} - {confidence}" 
+    labels = [f"{class_name} - {confidence:02f}" 
               for class_name, confidence
               in zip(class_names, detections.confidence)]
     
@@ -101,6 +115,33 @@ def on_prediction(alert_endpoint, inference_results, frame):
 
     annotated_frame = label.annotate(scene=frame.image.copy(), detections=detections, labels=labels)
     annotated_frame = bounding_box.annotate(scene=annotated_frame, detections=detections)
+
+    annotated_frame = sv.draw_text(
+        scene=annotated_frame,
+        text="Secure Desk v0.01",
+        text_anchor= sv.Point(x=700, y=100),
+        text_scale=3,
+        background_color=sv.Color(r=255, g=255, b=255),
+        text_color=sv.Color(r=0, g=0, b=0),
+    )
+
+    annotated_frame = sv.draw_text(
+        scene=annotated_frame,
+        text=alert_state_message,
+        text_anchor= sv.Point(x=1700, y=100),
+        text_scale=2,
+        background_color=sv.Color(r=75, g=0, b=130),
+        text_color=sv.Color(r=230, g=190, b=255),
+    )
+
+    annotated_frame = sv.draw_text(
+        scene=annotated_frame,
+        text=code_sequence_message,
+        text_anchor= sv.Point(x=1100, y=1200),
+        text_scale=2,
+        background_color=sv.Color(r=112, g=128, b=144),
+        text_color=sv.Color(r=204, g=255, b=0),
+    )
 
 
     if "person" in class_names:
@@ -133,8 +174,9 @@ def main():
     # Setup basic logging configuration
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+    config = toml.load("config.toml")
+
     # Load configuration file and parse values
-    config = toml.load('config.toml')
     rtsp_user = config["rtsp"]["user"]
     rtsp_pass = config["rtsp"]["password"]
     camera_ip = config["rtsp"]["camera_ip"]
@@ -149,7 +191,6 @@ def main():
     roboflow_model = config["app"]["roboflow_model"]
     roboflow_model = config["app"]["roboflow_model"]
     alert_endpoint = config["app"]["alert_endpoint"]
-
 
     # create a function partial to pass confi to callback.
     alert_config_partial = partial(on_prediction, alert_endpoint)
